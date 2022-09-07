5717fcdb4f9a02272017c549f50185b89b1f4adb
Luisdaries <luisdaries@gmail.com>
2022-09-06 23:24:59 -0500

    Make functions to get names of all sheets and convert to list

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Other necessary libaries
 https://openpyxl.readthedocs.io/en/stable/usage.html
 
 https://docs.python.org/es/3/library/tkinter.html
+
+https://pypi.org/project/pipreqs/
--- project/index.py
+++ project/index.py
@@ -1,31 +1,43 @@
-from operator import index
 import pandas as pd
 import numpy as np
 import openpyxl
 # Draw a new windows to reques the file yo get all data
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter import *     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
+
+# import module to print prettier
+import pprintpp
+
+# Import json library
+import json
+
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 file = filename
 
 
-data = pd.read_excel(file,sheet_name = 0, index_col=0)
+data = pd.read_excel(file, sheet_name = 0, index_col=0)
 
+# First function in start program
 def index():
-    getsheetname(file)
+    sheets = getsheetname(file)
+    converttolist(sheets)
+    printf(sheets)
 
 def getsheetname(file):
     sheets  = openpyxl.load_workbook(file)
-    print(sheets.sheetnames)
+    return sheets
+    
 
-def converttlist():
-    print()
-    # print(sheets.sheetnames)
-
-def printsheetnames():
-    print(sheets.sheetnames)
+def printf(sheets):
+    pprintpp.pprint(sheets.sheetnames)
 
 
+def converttolist(data):
+    listResult = list(data.sheetnames)
+    return listResult
 index()
+
+
+# data = pd.read_excel(file,sheet_name = "", index_col=0)
